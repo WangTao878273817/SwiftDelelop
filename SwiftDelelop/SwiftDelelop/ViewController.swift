152e368cf7e3646f6d8407bff05d77dbde252f4b
WangTao <878273817@qq.com>
2016-11-29 17:46:29 +0800

    init feil

---
+++ SwiftDelelop/SwiftDelelop/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  SwiftDelelop
+//
+//  Created by shoule on 2016/11/29.
+//  Copyright © 2016年 SaiDicaprio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("test001")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
